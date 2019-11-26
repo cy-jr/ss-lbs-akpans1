099b0a724abfbdcf886e3b14d1fd29ad590d154a
akpans1 <akpans1@udayton.edu>
2019-11-26 14:14:27 -0500

    Assg2: Fixed CSRF Vuln/del.php

--- assignments/assignment2/www.myblog.com/admin/del.php
+++ assignments/assignment2/www.myblog.com/admin/del.php
@@ -5,10 +5,21 @@
   require("../classes/db.php");
   require("../classes/phpfix.php");
   require("../classes/post.php");
+  $nocsrftoken = $_POST['nocsrftoken'];
 
 
-  $post = Post::find($_GET['id']);
+      if(!isset($nocsrftoken) or (!$nocsrftoken = $_POST['nocsrftoken'])){
+         echo "CSRF detected: No token from delete form!;";
+         die();
+      }
 
+      $post = Post::find($_GET['id']);
+
+?>
+
+<?php
+    $rand = bin2hex(openssl_random_pseudo_bytes(16));
+    $_SESSION["nocsrftoken"] = $rand;
 ?>
 
   <form action="delete.php?id=<?php echo htmlentities($_GET['id']);?>" method="POST" enctype="multipart/form-data">
@@ -18,21 +29,7 @@
       <textarea name="text" cols="80" rows="5">
         <?php echo htmlentities($post->text); ?>
         </textarea><br/>
-
+        <input type="hidden" name="nocsrftoken" value="<?php echo $rand; ?>"> 
     <input type="submit" name="Delete" value="Delete">
 
   </form>
-
-
-
-
-<!--   //generate random token
-    $rand = bin2hex(openssl_random_pseudo_bytes(16));
-    $_SESSION["nocsrftoken"] = $rand;
-
-      if(!isset($nocsrftoken) or (!$nocsrftoken = $_POST['nocsrftoken'])){
-         echo "CSRF detected: No token from edit page!;";
-         die();
-      }
-
-    <input type="hidden" name="nocsrftoken" value="<?php echo $rand; ?>"> -->
